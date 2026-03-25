5d9566aa5489886d92d93e8dc50a83799e7cd135
Pedro Jesus <pa04052007@gmail.com>
2026-03-25 00:53:43 -0300

    Delete .next/dev directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"K93wH7qIDLwU3zaxDM+7Pq6i8ZRQKphTJfTlxieefEY=\"\n}"
--- .next/dev/static/chunks/[hmr-entry]_hmr-entry_32f2e7e9.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[hmr-entry]/hmr-entry.js { ENTRY => \"[project]/Transferências/elos-creative/pages/_error\" }", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.r("[next]/entry/page-loader.ts { PAGE => \"[project]/node_modules/next/error.js [client] (ecmascript)\" } [client] (ecmascript)");
-}),
-]);
